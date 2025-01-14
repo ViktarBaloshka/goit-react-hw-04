70a130ec61b70ffb30df9598c1c5cc634bae594e
Viktar Baloshka <vboloshko@gmail.com>
2025-01-14 18:23:54 +0100

    init commit

---
+++ src/components/default-params.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+const API_KEY = "FHxvx8zol2InuS8C4YLb_Q3b-gycBq99ci0WrrEBYcU";
+/* axios.defaults.baseURL("https://api.unsplash.com/"); */
+export const params = {
+  key: API_KEY,
+};
